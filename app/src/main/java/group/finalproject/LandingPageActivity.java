a8a29121c1b25af782eaf5124ee692cac5ace48a
harryhum <harryhum22@gmail.com>
2018-11-30 10:42:27 -0500

    Added landing page toolbar.

--- app/src/main/java/group/finalproject/LandingPageActivity.java
+++ app/src/main/java/group/finalproject/LandingPageActivity.java
@@ -2,18 +2,27 @@ package group.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.content.Intent;
 
 import group.finalproject.food.FoodActivity;
+import group.finalproject.food.FoodFavourites;
 
 
-public class LandingPageActivity extends Activity {
+public class LandingPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Toolbar toolbar = findViewById(R.id.landingToolbar);
+        setSupportActionBar(toolbar);
 
         Button btn1 = findViewById(R.id.btnOne);
         Button btn2 = findViewById(R.id.btnTwo);
@@ -40,5 +49,37 @@ public class LandingPageActivity extends Activity {
             Intent nextScreen = new Intent(LandingPageActivity.this, BusActivity.class);
             startActivityForResult(nextScreen, 222);
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.landing_page, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.foodMenuItem:
+                Intent foodIntent = new Intent(LandingPageActivity.this, FoodActivity.class);
+                startActivityForResult(foodIntent, 555);
+                break;
+            case R.id.movieMenuItem:
+                Intent movieIntent = new Intent(LandingPageActivity.this, MovieActivity.class);
+                startActivityForResult(movieIntent, 333);
+                break;
+            case R.id.busMenuItem:
+                Intent busIntent = new Intent(LandingPageActivity.this, BusActivity.class);
+                startActivityForResult(busIntent, 222);
+                break;
+            case R.id.cbcMenuItem:
+                Intent cbcIntent = new Intent(LandingPageActivity.this, CBCActivity.class);
+                startActivityForResult(cbcIntent, 444);
+                break;
+        }
+        return true;
     }
 }
